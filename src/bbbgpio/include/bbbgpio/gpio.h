45005c0f1fceb39ef5a91f9effa99afea49bd8e5
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-04-18 05:53:01 -0700

    added led function.  closes #1

--- src/bbbgpio/include/bbbgpio/gpio.h
+++ src/bbbgpio/include/bbbgpio/gpio.h
@@ -1,39 +1,3 @@
-/*
- * SimpleGPIO.h
- *
- * Copyright Derek Molloy, School of Electronic Engineering, Dublin City University
- * www.derekmolloy.ie
- *
- * Based on Software by RidgeRun
- * Copyright (c) 2011, RidgeRun
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    This product includes software developed by the RidgeRun.
- * 4. Neither the name of the RidgeRun nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY RIDGERUN ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL RIDGERUN BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef SIMPLEGPIO_H_
 #define SIMPLEGPIO_H_
 
@@ -45,6 +9,8 @@
 #define SYSFS_ADC_DIR "/sys/bus/iio/devices/iio:device0"
 #define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
 #define MAX_BUF 64
+
+#define LED_DIR "/sys/class/leds/beaglebone:green:usr"
 
 enum PIN_DIRECTION{
 	INPUT_PIN=0,
@@ -59,6 +25,7 @@ enum PIN_VALUE{
 /****************************************************************
  * GPIO RELATED FUNCTIONS (DIGITAL)
  ****************************************************************/
+int led_set_value(unsigned int led, PIN_VALUE value); // expects an led value : [0,3]
 int gpio_export(unsigned int gpio);
 int gpio_unexport(unsigned int gpio);
 int gpio_set_dir(unsigned int gpio, PIN_DIRECTION out_flag);
--- src/bbbgpio/src/bbbgpio/gpio.cpp
+++ src/bbbgpio/src/bbbgpio/gpio.cpp
@@ -1,39 +1,3 @@
-/*
- * SimpleGPIO.cpp
- *
- * Modifications by Derek Molloy, School of Electronic Engineering, DCU
- * www.derekmolloy.ie
- * Almost entirely based on Software by RidgeRun:
- *
- * Copyright (c) 2011, RidgeRun
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    This product includes software developed by the RidgeRun.
- * 4. Neither the name of the RidgeRun nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY RIDGERUN ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL RIDGERUN BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "bbbgpio/gpio.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -43,25 +7,48 @@
 #include <fcntl.h>
 #include <poll.h>
 
+// expects an led value between 0 and 3
+int led_set_value(unsigned int led, PIN_VALUE value)
+{
+  int fd;
+  char buf[MAX_BUF];
+
+  snprintf(buf, sizeof(buf), LED_DIR "%d/brightness", led);
+
+  fd = open(buf, O_WRONLY);
+  if (fd < 0) {
+    perror("led/set-value");
+    return fd;
+  }
+
+  if (value==LOW)
+    write(fd, "0", 2);
+  else
+    write(fd, "1", 2);
+
+  close(fd);
+  return 0;
+}
+
 /****************************************************************
  * gpio_export
  ****************************************************************/
 int gpio_export(unsigned int gpio)
 {
-	int fd, len;
-	char buf[MAX_BUF];
+  int fd, len;
+  char buf[MAX_BUF];
 
-	fd = open(SYSFS_GPIO_DIR "/export", O_WRONLY);
-	if (fd < 0) {
-		perror("gpio/export");
-		return fd;
-	}
+  fd = open(SYSFS_GPIO_DIR "/export", O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/export");
+    return fd;
+  }
 
-	len = snprintf(buf, sizeof(buf), "%d", gpio);
-	write(fd, buf, len);
-	close(fd);
+  len = snprintf(buf, sizeof(buf), "%d", gpio);
+  write(fd, buf, len);
+  close(fd);
 
-	return 0;
+  return 0;
 }
 
 /****************************************************************
@@ -69,19 +56,19 @@ int gpio_export(unsigned int gpio)
  ****************************************************************/
 int gpio_unexport(unsigned int gpio)
 {
-	int fd, len;
-	char buf[MAX_BUF];
+  int fd, len;
+  char buf[MAX_BUF];
 
-	fd = open(SYSFS_GPIO_DIR "/unexport", O_WRONLY);
-	if (fd < 0) {
-		perror("gpio/export");
-		return fd;
-	}
+  fd = open(SYSFS_GPIO_DIR "/unexport", O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/export");
+    return fd;
+  }
 
-	len = snprintf(buf, sizeof(buf), "%d", gpio);
-	write(fd, buf, len);
-	close(fd);
-	return 0;
+  len = snprintf(buf, sizeof(buf), "%d", gpio);
+  write(fd, buf, len);
+  close(fd);
+  return 0;
 }
 
 /****************************************************************
@@ -89,24 +76,24 @@ int gpio_unexport(unsigned int gpio)
  ****************************************************************/
 int gpio_set_dir(unsigned int gpio, PIN_DIRECTION out_flag)
 {
-	int fd;
-	char buf[MAX_BUF];
+  int fd;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR  "/gpio%d/direction", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR  "/gpio%d/direction", gpio);
 
-	fd = open(buf, O_WRONLY);
-	if (fd < 0) {
-		perror("gpio/direction");
-		return fd;
-	}
+  fd = open(buf, O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/direction");
+    return fd;
+  }
 
-	if (out_flag == OUTPUT_PIN)
-		write(fd, "out", 4);
-	else
-		write(fd, "in", 3);
+  if (out_flag == OUTPUT_PIN)
+    write(fd, "out", 4);
+  else
+    write(fd, "in", 3);
 
-	close(fd);
-	return 0;
+  close(fd);
+  return 0;
 }
 
 /****************************************************************
@@ -114,24 +101,24 @@ int gpio_set_dir(unsigned int gpio, PIN_DIRECTION out_flag)
  ****************************************************************/
 int gpio_set_value(unsigned int gpio, PIN_VALUE value)
 {
-	int fd;
-	char buf[MAX_BUF];
+  int fd;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
 
-	fd = open(buf, O_WRONLY);
-	if (fd < 0) {
-		perror("gpio/set-value");
-		return fd;
-	}
+  fd = open(buf, O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/set-value");
+    return fd;
+  }
 
-	if (value==LOW)
-		write(fd, "0", 2);
-	else
-		write(fd, "1", 2);
+  if (value==LOW)
+    write(fd, "0", 2);
+  else
+    write(fd, "1", 2);
 
-	close(fd);
-	return 0;
+  close(fd);
+  return 0;
 }
 
 /****************************************************************
@@ -139,28 +126,28 @@ int gpio_set_value(unsigned int gpio, PIN_VALUE value)
  ****************************************************************/
 int gpio_get_value(unsigned int gpio, unsigned int *value)
 {
-	int fd;
-	char buf[MAX_BUF];
-	char ch;
+  int fd;
+  char buf[MAX_BUF];
+  char ch;
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
 
-	fd = open(buf, O_RDONLY);
-	if (fd < 0) {
-		perror("gpio/get-value");
-		return fd;
-	}
+  fd = open(buf, O_RDONLY);
+  if (fd < 0) {
+    perror("gpio/get-value");
+    return fd;
+  }
 
-	read(fd, &ch, 1);
+  read(fd, &ch, 1);
 
-	if (ch != '0') {
-		*value = 1;
-	} else {
-		*value = 0;
-	}
+  if (ch != '0') {
+    *value = 1;
+  } else {
+    *value = 0;
+  }
 
-	close(fd);
-	return 0;
+  close(fd);
+  return 0;
 }
 
 
@@ -170,20 +157,20 @@ int gpio_get_value(unsigned int gpio, unsigned int *value)
 
 int gpio_set_edge(unsigned int gpio, char *edge)
 {
-	int fd;
-	char buf[MAX_BUF];
+  int fd;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/edge", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/edge", gpio);
 
-	fd = open(buf, O_WRONLY);
-	if (fd < 0) {
-		perror("gpio/set-edge");
-		return fd;
-	}
+  fd = open(buf, O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/set-edge");
+    return fd;
+  }
 
-	write(fd, edge, strlen(edge) + 1);
-	close(fd);
-	return 0;
+  write(fd, edge, strlen(edge) + 1);
+  close(fd);
+  return 0;
 }
 
 /****************************************************************
@@ -192,16 +179,16 @@ int gpio_set_edge(unsigned int gpio, char *edge)
 
 int gpio_fd_open(unsigned int gpio)
 {
-	int fd;
-	char buf[MAX_BUF];
+  int fd;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
 
-	fd = open(buf, O_RDONLY | O_NONBLOCK );
-	if (fd < 0) {
-		perror("gpio/fd_open");
-	}
-	return fd;
+  fd = open(buf, O_RDONLY | O_NONBLOCK );
+  if (fd < 0) {
+    perror("gpio/fd_open");
+  }
+  return fd;
 }
 
 /****************************************************************
@@ -210,7 +197,7 @@ int gpio_fd_open(unsigned int gpio)
 
 int gpio_fd_close(int fd)
 {
-	return close(fd);
+  return close(fd);
 }
 
 // --------------------------------------------------------------
@@ -226,24 +213,24 @@ int gpio_fd_close(int fd)
  ****************************************************************/
 int adc_get_value(unsigned int adc, unsigned int *value)
 {
-	int fd;
-	char buf[MAX_BUF];
-	char retChars[5];
+  int fd;
+  char buf[MAX_BUF];
+  char retChars[5];
 
-	snprintf(buf, sizeof(buf), SYSFS_ADC_DIR "/in_voltage%d_raw", adc);
+  snprintf(buf, sizeof(buf), SYSFS_ADC_DIR "/in_voltage%d_raw", adc);
 
-	fd = open(buf, O_RDONLY);
-	if (fd < 0) {
-		perror("adc/get-value");
-		return fd;
-	}
+  fd = open(buf, O_RDONLY);
+  if (fd < 0) {
+    perror("adc/get-value");
+    return fd;
+  }
 
-	read(fd, retChars, 6);
+  read(fd, retChars, 6);
 
-	*value = strtol(retChars,NULL,0);
+  *value = strtol(retChars,NULL,0);
 
-	close(fd);
-	return 0;
+  close(fd);
+  return 0;
 }
 
 /****************************************************************
@@ -252,16 +239,16 @@ int adc_get_value(unsigned int adc, unsigned int *value)
 
 int adc_fd_open(unsigned int adc)
 {
-	int fd;
-	char buf[MAX_BUF];
+  int fd;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_ADC_DIR "/in_voltage%d_raw", adc);
+  snprintf(buf, sizeof(buf), SYSFS_ADC_DIR "/in_voltage%d_raw", adc);
 
-	fd = open(buf, O_RDONLY | O_NONBLOCK );
-	if (fd < 0) {
-		perror("adc/fd_open");
-	}
-	return fd;
+  fd = open(buf, O_RDONLY | O_NONBLOCK );
+  if (fd < 0) {
+    perror("adc/fd_open");
+  }
+  return fd;
 }
 
 /****************************************************************
@@ -270,5 +257,5 @@ int adc_fd_open(unsigned int adc)
 
 int adc_fd_close(int fd)
 {
-	return close(fd);
+  return close(fd);
 }
